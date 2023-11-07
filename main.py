87ab298a92ca87299f9b051d4f970efaaba8bfd9
zdupre18 <126183811+zdupre18@users.noreply.github.com>
2023-11-07 14:29:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,57 @@ def fib_recursive(n, counts):
     """    
     counts[n] += 1
     ###TODO
-    pass
+    counts[n] += 1
+    
+    # Base case
+    if n <= 1:
+        return n
+    
+    # Recursive case
+    return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
     
 
     
 def fib_top_down(n, fibs):
     ###TODO
-    pass
+    if fibs[n] is not None:
+        return fibs[n]
+
+    # Base case
+    if n <= 1:
+        fibs[n] = n
+    else:
+        # Recursive case
+        fibs[n] = fib_top_down(n - 1, fibs) + fib_top_down(n - 2, fibs)
+
+    return fibs[n]
 
 
 def fib_bottom_up(n):
     ###TODO
-    pass
+     if n <= 1:
+        return n
+
+    fibs = [0] * (n + 1)
+    fibs[1] = 1
+
+    for i in range(2, n + 1):
+        fibs[i] = fibs[i - 1] + fibs[i - 2]
+
+    return fibs[n]
 
 
 
 
 def fib_bottom_up_better(n):
     ###TODO
-    pass
+    if n <= 1:
+        return n
+
+    prev, curr = 0, 1
+
+    for i in range(2, n + 1):
+        next_fib = prev + curr
+        prev, curr = curr, next_fib
+
+    return curr
